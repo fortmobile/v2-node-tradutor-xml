5d2dbdd189d5c84aae6d72a08854908872d5ab59
MatheusMores <101605650+MatheusMores@users.noreply.github.com>
2022-11-09 14:31:33 -0300

    feedback

--- src/functions/Curitiba/traduzir-xml.ts
+++ src/functions/Curitiba/traduzir-xml.ts
@@ -19,6 +19,9 @@ import { Xml_Nfse } from '../../models/Xml-Nfse';
 
 async function traduzirXmlCuritiba(xml_file_path: string, folder_name: string){
     try {
+        var feedback: string[] = []
+
+
         var new_xml = await mudarDataTypes(xml_file_path);
 
         var parsed_xml = await parseXml(new_xml) as object;
@@ -26,39 +29,58 @@ async function traduzirXmlCuritiba(xml_file_path: string, folder_name: string){
 
 
         for(let i = 0; i < array_xmls.length; i++){
-            var valores_nfse = await getValoresNfse(array_xmls[i]) as ValoresNfse;
+            try{
+                var valores_nfse = await getValoresNfse(array_xmls[i]) as ValoresNfse;
 
-            var prestador = await getPrestador(array_xmls[i]) as Prestador;
-            console.log
-            var tomador = await getTomador(array_xmls[i]) as Tomador;
+                var prestador = await getPrestador(array_xmls[i]) as Prestador;
 
-            var servico = await getServico(array_xmls[i]) as Servico;
-            
-            var declaracao_prestacao = await getDeclaracao(array_xmls[i], servico, prestador, tomador);
+                var tomador = await getTomador(array_xmls[i]) as Tomador;
 
-            var orgao_gerador = await getOrgaoGerador(array_xmls[i]);
+                var servico = await getServico(array_xmls[i]) as Servico;
+                
+                var declaracao_prestacao = await getDeclaracao(array_xmls[i], servico, prestador, tomador);
 
-            var infnfse = await getInfNfse(array_xmls[i], valores_nfse, prestador, orgao_gerador, declaracao_prestacao);
-            
-            var xml_nfse = new Xml_Nfse(infnfse);
+                var orgao_gerador = await getOrgaoGerador(array_xmls[i]);
 
-            const builder = new XMLBuilder();
-            var xmlContent = builder.build(xml_nfse).toString()
-    
-            xmlContent = await xmlContent.replace('<Nfse>', '<Nfse versao="2.01">')
-            xmlContent = await xmlContent.replace('<ConsultarNfseServicoPrestadoResposta>', '<ConsultarNfseServicoPrestadoResposta xmlns="http://www.abrasf.org.br/nfse.xsd">')
-            xmlContent = await xmlContent.replace('<NfseCancelamento>', '<NfseCancelamento versao="2.01">')
-            
-            fs.writeFileSync(`${folder_name}/${prestador.IdentificacaoPrestador.CpfCnpj.Cnpj}-${infnfse.Numero}.xml`, xmlContent);
+                var infnfse = await getInfNfse(array_xmls[i], valores_nfse, prestador, orgao_gerador, declaracao_prestacao);
+                
+                var xml_nfse = new Xml_Nfse(infnfse);
+
+                const builder = new XMLBuilder();
+                var xmlContent = builder.build(xml_nfse).toString()
+        
+                xmlContent = await xmlContent.replace('<Nfse>', '<Nfse versao="2.01">')
+                xmlContent = await xmlContent.replace('<ConsultarNfseServicoPrestadoResposta>', '<ConsultarNfseServicoPrestadoResposta xmlns="http://www.abrasf.org.br/nfse.xsd">')
+                xmlContent = await xmlContent.replace('<NfseCancelamento>', '<NfseCancelamento versao="2.01">')
+                
+                fs.writeFileSync(`${folder_name}/${prestador.IdentificacaoPrestador.CpfCnpj.Cnpj}-${infnfse.Numero}.xml`, xmlContent);
+
+                feedback.push(`${prestador.IdentificacaoPrestador.CpfCnpj.Cnpj}-${infnfse.Numero}.xml convertido com sucesso`)
+            } 
+            catch(error){
+                var json_object = array_xmls[i]
+
+                type jsonKeys = keyof typeof json_object;
+                var base_path = json_object['Nfse' as jsonKeys]['InfNfse' as jsonKeys];
+                type jsonKeys2 = keyof typeof base_path;
+
+                var cnpj = base_path['PrestadorServico' as jsonKeys]['IdentificacaoPrestador' as jsonKeys2]['Cnpj' as jsonKeys2] as string;
+                cnpj = cnpj.replace('-', '').replace('.', '').replace('.', '').replace('/', '');
+
+                var numero = base_path['Numero' as jsonKeys2];
+
+                feedback.push(`${cnpj}-${numero} erro ao converter xml`)
+            }
         }
+        
 
         fs.unlinkSync(new_xml);
         
-        return({message: 'xml convertido com sucesso'});
+        return({message: feedback});
 
     } catch (error) {
 
-        return({message: 'erro ao converter xml'})
+        return({message: `erro ao converter xml`})
     }
 }
 
