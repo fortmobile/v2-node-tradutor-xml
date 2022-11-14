3579ff0b6ba910c17540d329d5e103ef2f1104b4
MatheusMores <101605650+MatheusMores@users.noreply.github.com>
2022-11-14 08:29:14 -0300

    rps

--- src/models/Rps.ts
+++ src/models/Rps.ts
@@ -4,22 +4,22 @@ class Rps{
         Serie: number,
         Tipo: number
     }
-    DataEmissao: string
+    DataEmissaoRps: string
     Status: number 
 
-    constructor(Numero: number, Serie: number, Tipo: number, DataEmissao: string, Status: number){
+    constructor(Numero: number, Serie: number, Tipo: number, DataEmissaoRps: string, Status: number){
         this.IdentificacaoRps = {
             Numero,
             Serie,
             Tipo
         }
-        DataEmissao
+        DataEmissaoRps
         Status
 
         this.IdentificacaoRps.Numero = Numero;
         this.IdentificacaoRps.Serie = Serie;
         this.IdentificacaoRps.Tipo = Tipo;
-        this.DataEmissao = DataEmissao;
+        this.DataEmissaoRps = DataEmissaoRps;
         this.Status = Status;
     }
 }
